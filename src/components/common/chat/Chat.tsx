c44d4904ff23ce4ffa61e5a6822a62886f38e2b6
Vladimir Liankevich <liankevichvladimir@gmail.com>
2022-04-27 00:04:32 +0300

    step_31 added onClick and onChange Handler for component NewPost and Chat new message

--- src/components/common/chat/Chat.tsx
+++ src/components/common/chat/Chat.tsx
@@ -1,5 +1,7 @@
 import './chat.scss';
 
+import { ChangeEvent, useState } from 'react';
+
 import Button from '../button/Button';
 import Message from '../message/Message';
 import TextareaText from '../textareaText/TextareaText';
@@ -11,6 +13,17 @@ type ChatPropsType = {
 const Chat = (props: ChatPropsType) => {
   const { messages = ['Hi', 'Let`s tell with you', 'Go'] } = props;
 
+  const [textarea, setTextarea] = useState<string | null>(null);
+  const textareaHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault;
+    const text = e.currentTarget.value;
+    setTextarea(text);
+  };
+
+  const addPostHandler = () => {
+    alert(textarea);
+  };
+
   const contentMessages = messages.map((mess, i) => (
     <Message key={i} message={mess} />
   ));
@@ -21,11 +34,14 @@ const Chat = (props: ChatPropsType) => {
         <div className='chat--messages'>{contentMessages}</div>
         <div className='chat--send'>
           <TextareaText
+            onChange={textareaHandler}
             border={true}
             placeholder='What’s on your mind?'
             name='new post'
           />
-          <Button className='chat--button'>send</Button>
+          <Button className='chat--button' onClick={addPostHandler}>
+            send
+          </Button>
         </div>
       </div>
     </section>
--- src/components/common/newPost/NewPost.tsx
+++ src/components/common/newPost/NewPost.tsx
@@ -1,20 +1,36 @@
 import './newPost.scss';
 
+import { ChangeEvent, useState } from 'react';
+
 import Button from '../button/Button';
 import TextareaText from '../textareaText/TextareaText';
 
 const NewPost = () => {
+  const [textarea, setTextarea] = useState<string | null>(null);
+  const textareaHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault;
+    const text = e.currentTarget.value;
+    setTextarea(text);
+  };
+
+  const addPostHandler = () => {
+    alert(textarea);
+  };
+
   return (
     <section className='new-post new-post-bg'>
       <div className='new-post--wrapper'>
         <span className='new-post--text'>New post</span>
         <div className='new-post--send'>
           <TextareaText
+            onChange={textareaHandler}
             border={true}
             placeholder='What’s on your mind?'
             name='new post'
           />
-          <Button className='new-post--button'>send</Button>
+          <Button className='new-post--button' onClick={addPostHandler}>
+            send
+          </Button>
         </div>
       </div>
     </section>
